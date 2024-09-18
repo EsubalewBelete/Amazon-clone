87b840fa64b0979390dff53711fee0df812177b8
EsubalewBelete <135066185+EsubalewBelete@users.noreply.github.com>
2024-09-18 11:41:07 -0700

    initial-commit

---
+++ vite-project/src/Component/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <section>
+        <section>
+            <div>
+                {/* logo */}
+                <a href="">
+                    <img src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'/>
+                </a>
+            </div>
+        </section>
+    </section>
+  )
+}
+
+export default Header
